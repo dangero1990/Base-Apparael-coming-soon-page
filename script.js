9fb288d1e982ef49fe9a44a5fc2ee62fdc411c8d
Andrew Eubanks <aeubanksguitar@gmail.com>
2023-01-20 23:18:50 -0500

    added more function to the error

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.getElementById("button").onclick = function() {
-    document.getElementById("error").innerHTML = "Please enter a valid email";
-    document.getElementById("email").style.border = "3px solid red"
+document.getElementById("button").onclick = function ValidateEmail() {
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(document.getElementById("email").value.match(mailformat)) {
+        return true;
+    } else {
+    document.getElementById("error__msg").innerHTML = "Please enter a valid email";
+    document.getElementById("error__icon").style.display = "block";
+    document.getElementById("email").style.border = "3px solid red";
+    return false;
+    }
 }
